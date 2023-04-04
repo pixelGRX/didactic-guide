a1724fd2073b713823ef63933ac3b0d32ed31fd2
pixelGRX <56277370+pixelGRX@users.noreply.github.com>
2023-04-04 18:23:38 +0300

    Update template.js

--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-const VERSION = 15
+const VERSION = 16
 
 const coordsEl = document.querySelector('div[title="Координаты пикселя."]')
 
@@ -16,7 +16,7 @@ let imgCtx, temCtx, imgW, imgH;
 
 async function loadTemplate(){
     return new Promise((res, rej) => {
-        const link = 'https://i.imgur.com/V2rHZHe.png';
+        const link = 'https://i.imgur.com/Ta9xw9C.png';
         const img = new Image();
 
         img.crossOrigin = 'anonymous';
